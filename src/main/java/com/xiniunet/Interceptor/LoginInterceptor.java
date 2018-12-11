92659a3f23b29c137acfb4aec81d11a9728c71db
zhangzhibin <zhangzhibin@xiniunet.com>
2018-12-11 18:18:47 +0800

    cookieUtils引入

--- src/main/java/com/xiniunet/Interceptor/LoginInterceptor.java
+++ src/main/java/com/xiniunet/Interceptor/LoginInterceptor.java
@@ -1,21 +1,33 @@
 package com.xiniunet.Interceptor;
 
 import com.xiniunet.domain.User;
+import com.xiniunet.utils.CookieUtils;
+import com.xiniunet.utils.JedisClient;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Component
 public class LoginInterceptor implements HandlerInterceptor {
+    @Autowired
+    private JedisClient jedisClient;
     /*
      * 进入controller层之前拦截请求
      */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         System.out.println("preHandle init");
+        //从拦截器中获取cookie的信息
+        String token = CookieUtils.getCookieValue("token");
+        if(StringUtils.isNotEmpty(token)){//如果存在token，则取redis中查看是否存在该记录
+            jedisClient.get("");
+        }
         request.setAttribute("user",new User());
         return HandlerInterceptor.super.preHandle(request, response,handler);
     }
